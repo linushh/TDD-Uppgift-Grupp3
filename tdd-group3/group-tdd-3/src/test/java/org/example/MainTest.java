572e8da924d73e3a2071fa82fdca141e8688d2ea
Linus Håkansson <Linus.Hakansson@yh.nackademin.se>
2023-09-20 11:28:19 +0200

    added test to git folder

---
+++ tdd-group3/group-tdd-3/src/test/java/org/example/MainTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void testBil(){
+        Bil bil = new Bil();
+        assertNotNull(bil);
+
+    }
+}
